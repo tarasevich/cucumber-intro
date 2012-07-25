cbd564dc77ef1d9d20f2eeff0290f0a388d3a483
Alexey Tarasevich <tarasevich.a@gmail.com>
2012-07-25 21:40:37 +0300

    generated rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberIntro::Application.config.secret_token = '9c09cde75a2f303b2919a299de38fbf2e87c8611696b879bcf9b7ad5134d6ebfbfc7c2325dbbb91b20a324fab211e9005b966cd8f24b1f64aae59c896c275c13'
